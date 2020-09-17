7e590c1bb43c7575411dc81bf146babf2aba40a6
AnamikaGautam52 <annu436gautam@gmail.com>
2020-09-17 17:54:09 +0000

    updated

---
+++ src/main/java/com/restapi/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.restapi.model.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import net.guides.springboot.crud.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, Long>{
+
+}
